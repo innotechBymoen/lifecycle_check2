3dcecbd387e70a96fdff6b9014c0107f83eff340
bymoen <alexb@innotechcollege.com>
2023-04-15 16:19:52 -0600

    Conditionals

--- src/components/TemplateFun.vue
+++ src/components/TemplateFun.vue
@@ -1,15 +1,28 @@
 <template>
     <div>
-        <h3>{{ blog_title }}</h3>
-        <p>{{ blog_content }}</p>
-        <img :src="blog_image_url" alt="Blog Image">
+        <button @click="toggle_post">TOGGLE POST</button>
+        <article v-if="show_post === true">
+            <button @click="hack_post">Hack Blog</button>
+            <h3>{{ blog_title }}</h3>
+            <p>{{ blog_content }}</p>
+            <img :src="blog_image_url" alt="Blog Image">
+        </article>
     </div>
 </template>
 
 <script>
     export default {
+        methods: {
+            hack_post: function() {
+                this.blog_content = this.blog_content + `I was injected`;
+            },
+            toggle_post: function() {
+                this.show_post = !this.show_post
+            }
+        },
         data() {
             return {
+                show_post: true,
                 blog_title: `Jeff Blog`,
                 blog_image_url: `https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80`,
                 blog_content: `We gotta burn the rain forest, dump toxic waste, pollute the air, and rip up the OZONE! 'Cause maybe if we screw up this planet enough, they won't want it anymore! We gotta burn the rain forest, dump toxic waste, pollute the air, and rip up the OZONE! 'Cause maybe if we screw up this planet enough, they won't want it anymore!`
