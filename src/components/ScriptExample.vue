691578b365f53cdadedc0c1226031db17fc5b8d8
bymoen <alexb@innotechcollege.com>
2023-04-15 13:05:50 -0600

    Variables

---
+++ src/components/ScriptExample.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>I AM THE SCRIPT EXAMPLE</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                tweets: [`This is the first tweet`,`Second Tweet`,`This is the last tweet`],
+                num_friends: 2,
+                logged_in: true
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
