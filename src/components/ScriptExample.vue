ebe3aa72aa3f5d550a390faa38b8a2ffb266cb25
bymoen <alexb@innotechcollege.com>
2023-04-15 13:52:41 -0600

    Functions

--- src/components/ScriptExample.vue
+++ src/components/ScriptExample.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <h1>I AM THE SCRIPT EXAMPLE</h1>
+        <div class="script_container"></div>
     </div>
 </template>
 
@@ -10,9 +11,26 @@
             return {
                 tweets: [`This is the first tweet`,`Second Tweet`,`This is the last tweet`],
                 num_friends: 2,
-                logged_in: true
+                logged_in: false,
             }
         },
+        methods: {
+            show_info: function() {
+                let script_container = document.querySelector(`.script_container`);
+                script_container.insertAdjacentHTML(`beforeend`, `<p>Friends: ${this.num_friends}</p>`);
+                if(this.logged_in === true) {
+                    script_container.insertAdjacentHTML(`beforeend`, `<p>You are logged in!</p>`);
+                } else {
+                    script_container.insertAdjacentHTML(`beforeend`, `<p>You are NOT logged in!</p>`);
+                }
+                for(let i=0; i<this.tweets.length; i++) {
+                    script_container.insertAdjacentHTML(`beforeend`, `<p>${this.tweets[i]}</p>`);
+                }
+            }
+        },
+        mounted: function(){
+            this.show_info();
+        }
     }
 </script>
 
