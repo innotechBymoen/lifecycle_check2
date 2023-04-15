f267080f1749318e2b0a0f4c7f1032d0280e5e04
bymoen <alexb@innotechcollege.com>
2023-04-15 11:27:09 -0600

    Showed the life cycle

---
+++ src/components/LifeCycleExample.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h1>Example of Life Cycle Component</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted: function() {
+            console.log(`I am in mounted`);
+        },
+        beforeCreate: function() {
+            console.log(`I am in beforeCreate`);
+        },
+        beforeMount: function() {
+            console.log(`I am in beforeMount`);
+        },
+        created: function() {
+            console.log(`I am in created`);
+        },
+    }
+</script>
+
+<style scoped>
+    h1 {
+        color: red;
+    }
+</style>
